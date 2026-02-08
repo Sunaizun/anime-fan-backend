c6731fbfecee9a05c7fdd56a13b41557f04b6db0
Sunaizun <eroshka.ty@gmail.com>
2026-02-08 21:11:52 +0500

    Add controllers

--- controllers/authController.js
+++ controllers/authController.js
@@ -26,21 +26,18 @@ exports.register = async (req, res, next) => {
 };
 
 exports.login = async (req, res) => {
-  const { email, password } = req.body; 
-    try {
+  const { email, password } = req.body;
+  try {
     const user = await User.findOne({ email });
     if (!user) return res.status(400).json({ message: 'Invalid credentials' });
-    const isMatch = await bcrypt.compare(password, user.password);  
+
+    const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) return res.status(400).json({ message: 'Invalid credentials' });
+
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.json({ token });
+    return res.json({ token });
   } catch (err) {
     console.error(err);
-    res.status(500).json({ message: 'Server error' });
+    return res.status(500).json({ message: 'Server error' });
   }
-
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-  res.json({ token });
-
-
 };
