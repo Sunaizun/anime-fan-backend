c99957addfc9c22f4cae3904f2af14e9bc3a0094
Sunaizun <eroshka.ty@gmail.com>
2026-02-08 21:09:44 +0500

    Add Posts CRUD

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ app.use('/api/posts', require('./routes/postRoutes'));
 
 
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
 
